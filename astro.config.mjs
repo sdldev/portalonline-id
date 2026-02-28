f441c8b0ed502ab852853463dbf576504dae6fb3
sdldev <sidolapakdev@gmail.com>
2026-02-28 22:27:58 +0700

    implement astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,36 +8,51 @@ import mdx from '@astrojs/mdx';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portalonline.id',
   output: 'server',
-  integrations: [
-    sitemap({
-      /** @param {any} item */
-      serialize(item) {
-        if (item.url === 'https://portalonline.id/') {
-          item.changefreq = 'daily';
-          item.priority = 1.0;
-        } else if (item.url.match(/https:\/\/portalonline\.id\/[^\/]+\/[^\/]+/)) {
-          // Inner items (e.g. /article/something)
-          item.changefreq = 'weekly';
-          item.priority = 0.8;
-        } else if (item.url.match(/about|contact|pricing|privacy|terms|seo|themes/)) {
-          // Static support pages
-          item.changefreq = 'monthly';
-          item.priority = 0.5;
-        } else {
-          // Category index pages like /article/, /tips/, /author/
-          item.changefreq = 'daily';
-          item.priority = 0.9;
-        }
-        item.lastmod = new Date().toISOString();
-        return item;
+  integrations: [sitemap({
+    /** @param {any} item */
+    serialize(item) {
+      if (item.url === 'https://portalonline.id/') {
+        item.changefreq = 'daily';
+        item.priority = 1.0;
+      } else if (item.url.match(/https:\/\/portalonline\.id\/[^\/]+\/[^\/]+/)) {
+        // Inner items (e.g. /article/something)
+        item.changefreq = 'weekly';
+        item.priority = 0.8;
+      } else if (item.url.match(/about|contact|pricing|privacy|terms|seo|themes/)) {
+        // Static support pages
+        item.changefreq = 'monthly';
+        item.priority = 0.5;
+      } else {
+        // Category index pages like /article/, /tips/, /author/
+        item.changefreq = 'daily';
+        item.priority = 0.9;
       }
-    }),
-    mdx()
-  ],
+      item.lastmod = new Date().toISOString();
+      return item;
+    }
+  }), mdx(), playformCompress({
+    CSS: true,
+    HTML: {
+      "html-minifier-terser": {
+        removeAttributeQuotes: false,
+      },
+    },
+    JavaScript: true,
+    SVG: true,
+    Exclude: [
+      "\\.webp$",
+      "\\.avif$",
+      "\\.woff2$",
+      "\\.woff$",
+    ],
+    Logger: 1,
+  })],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
