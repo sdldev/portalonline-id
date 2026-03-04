6c6a95aef1fe930f1036d272318aaa2391d2b449
sdldev <sidolapakdev@gmail.com>
2026-03-05 01:26:01 +0700

    export const prerender = true;

---
+++ src/pages/api/test-ttfb.js
@@ -0,0 +1,10 @@
+export const prerender = false;
+
+export async function GET() {
+    return new Response(JSON.stringify({ message: "Hello from API!" }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
